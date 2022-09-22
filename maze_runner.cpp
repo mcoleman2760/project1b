d68bb104478d32b4db404a7609e1a69cd14f4682
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2022-09-22 14:56:48 -0400

    Create maze_runner.cpp

---
+++ maze_runner.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include<string>
+
+int main() {
+
+  int maze[6][6] = {
+   1,0,1,1,1,1,
+   1,0,0,0,1,1,
+   1,0,1,0,0,1,
+   1,0,1,0,1,1,
+   1,0,0,0,0,0,
+   1,1,1,1,1,1 };
+  char s;
+// std::string s;
+  int r = 0;
+  int c = 1;
+ 
+
+    
+  while(std::cin >> s) {
+    if (s =='R' && maze[r][c+1] == 0){
+    c++;
+      }
+    if (s =='L' && maze[r][c-1] == 0){
+    c--;
+      }
+    if (s =='D' && maze[r+1][c] == 0){
+    r++;
+      }
+    if (s =='U' && maze[r-1][c] == 0){
+    r--;
+      }
+
+ 
+  
+    }
+  if (r == 4 && c == 5){
+    std::cout << "You got out of the maze \n" ;
+  }
+  else {
+    std::cout << "You got stuck in the maze \n" ;
+  }
+  }
